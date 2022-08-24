f6badf3c9a5c7a88ad798007b759e608a7b72cb3
cransters <108357630+cransters@users.noreply.github.com>
2022-08-23 21:41:42 -0500

    ingresoegreso

---
+++ ingresoegreso/src/main/java/co/gov/mintic/cartera/ingresoegreso/entity/Rol.java
@@ -0,0 +1,8 @@
+package co.gov.mintic.cartera.ingresoegreso.entity;
+
+public class Rol {
+
+    private long idRol;
+    private String descripcion;
+    private boolean estado;
+}
---
+++ ingresoegreso/src/main/java/co/gov/mintic/cartera/ingresoegreso/entity/TipoDocumento.java
@@ -0,0 +1,9 @@
+package co.gov.mintic.cartera.ingresoegreso.entity;
+
+public class TipoDocumento {
+
+    private long idTipoDocumento;
+    private String descripcion;
+    private String siglas;
+    private boolean estado;
+}
